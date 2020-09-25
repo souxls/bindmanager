1319f28c9324a21f0b87f8986d34df8a5a84e0f3
souxls <souxls@hotmail.com>
2020-09-25 10:48:55 +0800

    .

--- application/__about__.py
+++ application/__about__.py
@@ -2,5 +2,5 @@ __title__ = 'bindmanager'
 __description__ = 'web manager of dns'
 __version_info__ = ('0', '1', '0')
 __version__ = '.'.join(__version_info__)
-__author__ = 'dengxiaoshan'
-__author_email__ = 'dengxiaoshan@pwrd.com'
+__author__ = 'souxls'
+__author_email__ = 'souxls@hotmail.com'
